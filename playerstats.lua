20b1805b477caaf4356f0871eeae20f5a26004aa
geiz <dxs@hotmail.ca>
2014-04-09 04:01:27 -0400

    Code restructured to be more intuitive. Activemining and sliding to change img works

---
+++ playerstats.lua
@@ -0,0 +1,14 @@
+-- player.lua
+
+local playerstats = {
+	name = "george",
+	dollar = 0,
+	gem = 0,
+	pickaxe_power = 1,
+	pickaxe_quality = 1,
+	active_multiplier = 1, -- Bonus multiplier for active player clicks
+	passive_multiplier = 1, -- Bonus multiplier for passive player clicks
+
+}
+
+return playerstats
